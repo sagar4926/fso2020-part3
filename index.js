034ced984c21efa88fbd428bd64b5d14df5f8dfc
Sagar B <sagar4926@gmail.com>
2020-04-11 15:40:23 +0530

    3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 3030;
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
